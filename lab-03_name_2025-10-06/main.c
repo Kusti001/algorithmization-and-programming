dfc422ae4671a6ead0cfa53b76db70373722801f
Kusti001 <galimoff.sergei@gmail.com>
2025-10-09 20:24:16 +0300

    final

---
+++ lab-03_name_2025-10-06/main.c
@@ -0,0 +1,101 @@
+#define CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void swap(int *pv) {
+    int t = pv[0]; pv[0] = pv[1]; pv[1]=t;
+}
+
+int Ar[2];
+
+void quick_sort(int ar[], int l, int r) {
+    if (l < r) {
+        int pivot = ar[r];
+        int i = l - 1;
+        for (int j = l; j < r; j++) {
+            if (ar[j] < pivot) {
+                i++;
+                int tmp = ar[i];
+                ar[i] = ar[j];
+                ar[j] = tmp;
+            }
+        }
+        int tmp = ar[i + 1];
+        ar[i + 1] = ar[r];
+        ar[r] = tmp;
+
+        int pi = i + 1;
+        quick_sort(ar, l, pi - 1);
+        quick_sort(ar, pi + 1, r);
+    }
+}
+
+double find_median(int ar[], int n) {
+    int count[n];
+    for (int i = 0; i < n; i++) {
+        count[i] = 0;
+        for (int j = 0; j < n; j++) {
+            if (ar[j] < ar[i])
+                count[i]++;
+        }
+    }
+    if (n % 2 == 1) {
+        int mid = n / 2;
+        for (int i = 0; i < n; i++)
+            if (count[i] == mid)
+                return ar[i];
+    } else {
+        int m1 = -1, m2 = -1;
+        for (int i = 0; i < n; i++) {
+            if (count[i] == n/2 - 1) m1 = i;
+            if (count[i] == n/2) m2 = i;
+        }
+        return (ar[m1] + ar[m2]) / 2.0;
+    }
+}
+
+int main(void) {
+    time_t curtime;
+    time(&curtime);
+    srand((unsigned int) curtime);
+
+    int n = sizeof(Ar) / sizeof(Ar[0]);
+    for (int i = 0; i < n; i++) {
+        Ar[i] = rand() % 1000;
+    }
+
+
+    for (int i = 0; i < n; i++){
+        printf("%d\n", Ar[i]);
+    }
+
+    /*for (int i = 0; i < n; i++) {
+        for (int j = 0; j < (n -1); j++) {
+            if (Ar[j] < Ar[j + 1]) {
+                swap(&Ar[j]);
+                flag = 1;
+            }
+        }
+        if (flag == 0) break;
+    } */
+
+    quick_sort(Ar, 0, n - 1);
+
+    printf("\n");
+    char ch = ',';
+    for (int i = 0; i < n; i++) {
+        if (i == n-1) ch = ' ';
+        printf("%05d%c", Ar[i],ch);
+    }
+
+    double med = find_median(Ar, n);
+    printf("\nМедиана: %.2f\n", med);
+
+    return 0;
+}
+
+
+//1. заменить алгоритм сортировки +
+//2. найти медиану      *без сортировки
+//3. размер массива любой + генератор случайных чисел впихнуть +
