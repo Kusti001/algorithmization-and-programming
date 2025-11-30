5eb7f36d7c1aed7c2825bec59a842095ed9824af
kusti <galimoff.sergei@gmail.com>
2025-11-30 23:15:11 +0300

    upd memroy leak

--- lab-06_files_2025-11-10/main.c
+++ lab-06_files_2025-11-10/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+
+int size = 2;
 
 int is_snake(const char *str) {
     int cnt = 0;
@@ -24,13 +25,11 @@ int is_camel(const char *str) {
 
 char *snake_to_camel(const char *buffer, const int sz) {
     char *new_buffer = malloc(sizeof(char) * (sz + 1 - is_snake(buffer)));
-    int pos = 0;
+    int pos = 1;
     int flag = 0;
-    for (int i = 0; buffer[i] != '\0'; i++) {
-        if (i == 0) {
-            new_buffer[pos] = buffer[i];
-            pos++;
-        } else if (buffer[i] == '_') {
+    new_buffer[0] = buffer[0];
+    for (int i = 1; buffer[i] != '\0'; i++) {
+        if (buffer[i] == '_') {
             flag = 1;
         } else if (flag == 1) {
             new_buffer[pos] = buffer[i] - 32;
@@ -46,7 +45,7 @@ char *snake_to_camel(const char *buffer, const int sz) {
     return new_buffer;
 }
 
-char *camel_to_snake(const char *buffer, int sz) {
+char *camel_to_snake(const char *buffer, const int sz) {
     char *new_buffer = malloc(sizeof(char) * (sz +1 + is_camel(buffer)));
     int pos = 0;
     for (int i = 0; buffer[i] != '\0'; i++) {
@@ -85,6 +84,7 @@ void check_str(char *buffer, const int sz, FILE *end_file) {
             printf("%s - > ERROR (two types of codestyle)\n",buffer);
             return;
         }
+        //обработка snake to camel
         char *result = snake_to_camel(buffer, sz);
         fputs(result, end_file);
         fputs("\n", end_file);
@@ -92,8 +92,9 @@ void check_str(char *buffer, const int sz, FILE *end_file) {
         free(result);
         return;
     }
-
+    // если camel
     if (is_camel(buffer)) {
+        //обработка
         char *result = camel_to_snake(buffer, sz);
         fputs(result, end_file);
         fputs("\n", end_file);
@@ -101,33 +102,34 @@ void check_str(char *buffer, const int sz, FILE *end_file) {
         free(result);
         return;
     }
-
+    //если ничего не подошло, ничего не меняем
     fputs(buffer, end_file);
     fputs("\n", end_file);
     printf("%s - > %s (nothing)\n", buffer, buffer);
 }
-int size = 0;
 char * convert_str(char * buffer,FILE *start_file, FILE *end_file, const long start_pos, const long end_pose, const int len) {
-    if (len > size) {
-        size = len;
-        buffer = realloc(buffer, size);
-        if (buffer == NULL) {
+    if (len > size) { //если текущий размер строки больше, увеличиваем буфер
+        size = len + 1;//новый размер
+        char *new_buffer = realloc(buffer, size);//переоткрываем буфер
+        if (new_buffer == NULL){//если не нашлось памяти
+            free(buffer);
             return NULL;
         }
+        buffer = new_buffer;
     }
 
-    fseek(start_file, start_pos, SEEK_SET);
-    fread(buffer, sizeof(char), len, start_file);
-    buffer[len] = '\0';
+    fseek(start_file, start_pos, SEEK_SET); //ставим картеку на начало слова
+    fread(buffer, sizeof(char), len, start_file); //записываем слово в буфер
+    buffer[len] = '\0'; //добавляем конец строки
 
-    check_str(buffer, len, end_file);
+    check_str(buffer, len, end_file); //операция со строкой и запись
 
-    fseek(start_file, end_pose, SEEK_SET);
-    return buffer;
+    fseek(start_file, end_pose, SEEK_SET); //возврат каретки
+    return buffer; //вернули указатель на буфер
 }
 
 int main() {
-    FILE *start_file = fopen("1.txt", "rb");
+    FILE *start_file = fopen("1.txt", "r"); //читаем
     FILE *end_file = fopen("2.txt", "w");
 
     if (!start_file) {
@@ -141,42 +143,28 @@ int main() {
     printf("file start:\n\n");
 
     int c;
-
-    /*
-    while ((c = fgetc(start_file)) != EOF) {
-        // Печатаем ВСЕ символы понятно
-        if (c == 10) {
-            printf("\\n");
-        } else if (c == 13) {
-            printf("\\r");
-        } else if (c >= 32 && c <= 126) {
-            printf("%c", c);
-        } else {
-            printf("[%d]", c);
-        }
-
-        // Обрабатываем конец строки Windows (\r\n)
-        if (c == '\n') {
-            end_pos = ftell(start_file);
-            printf(" start=%ld, end=%ld, length=%ld\n", start_pos, end_pos, end_pos - start_pos);
-            start_pos = end_pos;
-        }
-    }
-    end_pos = ftell(start_file);
-    printf(" start=%ld, end=%ld, length=%ld\n",start_pos, end_pos, end_pos - start_pos);
-*/
-    char *buffer = malloc(sizeof(char) * (2));
+    char *buffer = malloc(sizeof(char) * size); //выделяем буфер для строк
     if (buffer == NULL) {
         return 1;
     }
     while ((c = fgetc(start_file)) != EOF) {
-        if (c == '\n') {
-            end_pos = ftell(start_file);
-            buffer = convert_str(buffer,start_file, end_file, start_pos, end_pos, end_pos - start_pos - 1);
-            start_pos = end_pos;
+        if (c == '\n') { //если пришли в конец строки
+            end_pos = ftell(start_file); //записываем позицию каретки
+            buffer = convert_str(buffer,start_file, end_file, start_pos, end_pos, end_pos - start_pos - 2); //обрабатываем
+
+            if (!buffer) { //если не получилось открыть
+
+                fclose(start_file);
+                fclose(end_file);
+                return 1;
+            }
+
+            start_pos = end_pos;//теперь конец - начало
+
+
         }
     }
-    end_pos = ftell(start_file);
+    end_pos = ftell(start_file); //обрабатываем последнюю строку, в ней нет \r\n
     buffer = convert_str(buffer,start_file, end_file, start_pos, end_pos, end_pos - start_pos);
 
     free(buffer);
@@ -209,4 +197,28 @@ a_b_c_d // aBCD
 
 
 мысль - хранить указатель на начало строки и двигать его пока не дойдём до /n
+*/
+
+/*
+while ((c = fgetc(start_file)) != EOF) {
+    // Печатаем ВСЕ символы понятно
+    if (c == 10) {
+        printf("\\n");
+    } else if (c == 13) {
+        printf("\\r");
+    } else if (c >= 32 && c <= 126) {
+        printf("%c", c);
+    } else {
+        printf("[%d]", c);
+    }
+
+    // Обрабатываем конец строки Windows (\r\n)
+    if (c == '\n') {
+        end_pos = ftell(start_file);
+        printf(" start=%ld, end=%ld, length=%ld\n", start_pos, end_pos, end_pos - start_pos);
+        start_pos = end_pos;
+    }
+}
+end_pos = ftell(start_file);
+printf(" start=%ld, end=%ld, length=%ld\n",start_pos, end_pos, end_pos - start_pos);
 */
